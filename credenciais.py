f75d7befbd77c707cc252f33e7d79a1fb8eb2be8
carlos silvano morais <127549934+carlosmorais-git@users.noreply.github.com>
2024-12-09 08:37:50 -0300

    Update credenciais.py

--- credenciais.py
+++ credenciais.py
@@ -1,6 +1,6 @@
-remetente = 'silvanoanalista@gmail.com'
-destinatario = 'carlos.morais.1642@gmail.com'
+remetente = ''
+destinatario = ''
 
 # Obtém a senha do remetente. 
 # Essa senha é encontrada no gmail do remetente pesquise por "Suas senhas de app" cria e cole aqui
-senha = 'nmzi sati heqx wdnl'
+senha = ''
