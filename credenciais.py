8560d6e49f8ce1722838575db7027d0328f4dc0a
carlos silvano morais <127549934+carlosmorais-git@users.noreply.github.com>
2024-12-09 08:35:16 -0300

    Deploy inicial

---
+++ credenciais.py
@@ -0,0 +1,6 @@
+remetente = 'silvanoanalista@gmail.com'
+destinatario = 'carlos.morais.1642@gmail.com'
+
+# Obtém a senha do remetente. 
+# Essa senha é encontrada no gmail do remetente pesquise por "Suas senhas de app" cria e cole aqui
+senha = 'nmzi sati heqx wdnl'
