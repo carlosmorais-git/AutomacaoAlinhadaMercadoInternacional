c2510f897bd14500adbb728854757884cecd24ea
carlos silvano morais <127549934+carlosmorais-git@users.noreply.github.com>
2024-12-09 11:12:09 -0300

    deploy

--- credenciais.py
+++ credenciais.py
@@ -2,5 +2,5 @@ remetente = ''
 destinatario = ''
 
 # Obtém a senha do remetente. 
-# Essa senha é encontrada no gmail do remetente pesquise por "Suas senhas de app" cria e cole aqui
+# Essa senha é encontrada no gmail do remetente, pesquise por: "Suas senhas de app" cria e cole aqui
 senha = ''
